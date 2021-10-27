dcda6bd309f6e35efd4e5ca9ac0105b049b486c9
syarawldn <waldanasyara@gmail.com>
2021-10-27 17:43:56 +0700

    pertemuan 9

--- application/models/ModelUser.php
+++ application/models/ModelUser.php
@@ -7,17 +7,17 @@ class ModelUser extends CI_Model
     {
         $this->db->insert('user', $data);
     }
-
+    
     public function cekData($where = null)
     {
         return $this->db->get_where('user', $where);
     }
-
+    
     public function getUserWhere($where = null)
     {
         return $this->db->get_where('user', $where);
     }
-
+    
     public function cekUserAccess($where = null)
     {
         $this->db->select('*');
@@ -25,7 +25,7 @@ class ModelUser extends CI_Model
         $this->db->where($where);
         return $this->db->get();
     }
-
+    
     public function getUserLimit()
     {
         $this->db->select('*');
@@ -33,5 +33,4 @@ class ModelUser extends CI_Model
         $this->db->limit(10, 0);
         return $this->db->get();
     }
-
 }
