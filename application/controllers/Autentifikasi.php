ac11ac6c9dbfcd74b05acabfa92c45dc1ded9cb4
syarawldn <waldanasyara@gmail.com>
2021-10-27 17:51:39 +0700

    Update Autentifikasi

--- application/controllers/Autentifikasi.php
+++ application/controllers/Autentifikasi.php
@@ -135,7 +135,7 @@ private function _login()
                     'image'         => 'default.jpg',
                     'password'      => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
                     'role_id'       => 2,
-                    'is_active'     => 0,
+                    'is_active'     => 1,
                     'tanggal_input' => time()
                 ];
     
